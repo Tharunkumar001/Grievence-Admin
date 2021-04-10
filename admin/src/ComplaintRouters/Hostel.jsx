58a1c4d172648c31a0c8303fe71336fba010d441
Tharunkumar001 <tharung1411@gmail.com>
2021-04-10 11:30:24 +0530

    dynamic render

--- admin/src/ComplaintRouters/Hostel.jsx
+++ admin/src/ComplaintRouters/Hostel.jsx
@@ -4,36 +4,52 @@ import axios from 'axios'
 
 const HostelContainer = () => {
     const [hostels,setHostels] = useState(["Testing"])
-    const [mail,setMail] = useState("");
+    const [mail,setMail] = useState(["tharung1411@gmail.com"]);
     // const [time,setTime] = useState(["12/1/2020"])
     // const [host,setHost] = useState([])
+    // useEffect(() => {
+      
+
+    //     axios.put("http://localhost:4000/getHostelComplaints").then((res)=>{
+    //         var arrayForMail = []
+    //         for (let index = 0; index <res.data.length; index++) {
+    //             arrayForMail.push(res.data[index].email)
+                        
+    //         }
+    //         setMail(arrayForMail);
+    //     })
+       
+    // })
 
     useEffect(() => {
         axios.put("http://localhost:4000/getHostelComplaints").then((res)=>{
        
-            var array = []
-
+            var array =[]
+            
           
 
             for (let index = 0; index < res.data.length; index++) {
-                // setMail(res.data[index].email);
+             
                 for (let index1 =  (res.data[index].comp.length) - 1; index1 >= 0 ; index1--) {
-                    array.push(res.data[index].comp[index1]);          
+        
+                    array.push({"mail":res.data[index].email,"comp":res.data[index].comp[index1]})
+                    // array.push({"comp":res.data[index].comp[index1]});  
+                            
                 }
         }
 
   
             setHostels(array)
-
-    
+            // console.log(array)
+            
         })
     })
   
         return(
             <div className="div">
-                {hostels.map((hos,i)=> <AdminTab key={i} Email={mail} comp={hos} brand={"Hostels"} />)}
+                {hostels.map((hos,i)=> <AdminTab key={i} mail={hos.mail} comp={hos.comp} brand={"hostel"} />)}
                
-                
+                {/* {mail.map((em,j) => <AdminTab key={j}  mail={em} />)} */}
             </div>
         )
 }
